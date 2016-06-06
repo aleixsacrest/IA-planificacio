7ef0dbd68ce6fe55ca87618484b24b9f15674787
aleixsacrest <aleixsacrest@gmail.com>
2016-06-06 13:00:01 +0200

    py

--- genProblema.py
+++ genProblema.py
@@ -1,5 +1,81 @@
-dies = ''
+#!/usr/bin/python
+
+from sets import Set
+import random
+
+objects = ''
+init = ''
+setOps = Set([])
+
+dies = ['','']
 for i in range(1,16):
-	dies += 'dia' + str(i) + ' '
-	
-print dies
+	d = 'dia' + str(i)
+	dies[0] += d + ' '
+	op = '(dia'
+	dies[1] += op + ' ' + d + ') '
+	setOps.add(op+' ?x)')
+	if i > 1:
+		op = '(dia-seguent'
+		dies[1] += op + ' dia' + str(i-1) + ' dia' + str(i) + ') '
+		setOps.add(op+' ?x ?y)')
+objects += '\n' + dies[0]
+init += '\n' + dies[1]
+
+difi = ['','']
+for i in range(1,11):
+	n = 'niv' + str(i)
+	difi[0] += n + ' '
+	op = '(dificultat'
+	dies[1] += op + ' ' + n + ') '
+	setOps.add(op+' ?x)')
+	if i > 1:
+		op = '(dif-seguent'
+		difi[1] += op + ' niv' + str(i-1) + ' niv' + str(i) + ') '
+		setOps.add(op+' ?x ?y)')
+objects += '\n' + difi[0]
+init += '\n' + difi[1]
+
+ex = ['','']
+ex_rel = {}
+nex = int(raw_input('nombre d\'exercicis: '))
+npred = 0
+nprep = 1
+for i in range(0,nex):
+	nom = 'ex' + str(i)
+	ex[0] += nom + ' '
+	op = '(exercici'
+	dies[1] += op + ' ' + nom + ') '
+	setOps.add(op+' ?x)')
+	dificultat = random.randint(1,10)
+	op = '(ultima-dificultat'
+	ex[1] += op + ' ' + nom + ' niv' + str(dificultat) + ') '
+	setOps.add(op+' ?x ?y)')
+	objectiu = random.randint(dificultat,10)
+	op = '(objectiu'
+	ex[1] += op + ' ' + nom + ' niv' + str(objectiu) + ') '
+	setOps.add(op+' ?x ?y)')
+	ex_rel[nom] = [[],[]]
+	npredi = random.randint(0,npred)
+	nprepi = random.randint(0,nprep)
+	for j in range(0,npredi):
+		p = 'ex'+str(random.randint(0,nex))
+		while (p in ex_rel.keys() and (nom in ex_rel[p][0] or nom in ex_rel[p][1])):
+			p = 'ex'+str(str(random.randint(0,nex)))
+		ex_rel[nom][0].append(p)
+		op = '(predecessor'
+		ex[1] += op + nom + p + ') '
+		setOps.add(op+' ?x ?y)')
+	for j in range(0,nprepi):
+		p = 'ex'+str(random.randint(0,nex))
+		while (p in ex_rel.keys() and (nom in ex_rel[p][0] or nom in ex_rel[p][1])):
+			p = 'ex'+str(random.randint(0,nex))
+		ex_rel[nom][1].append(p)
+		op = '(preparador'
+		ex[1] += op + ' ' + nom + ' ' + p + ') '
+		setOps.add(op+' ?x ?y)')
+objects += '\n' + ex[0]
+init += '\n' + ex[1]
+
+print objects
+print init
+for s in setOps: print s
